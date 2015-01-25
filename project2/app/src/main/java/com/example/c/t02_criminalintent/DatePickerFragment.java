dfebe76ed5fa2db38de0d99f97b35ae13bfddd28
sj cho <juns0201@gmail.com>
2015-01-25 11:33:50 +0900

    datefragment

---
+++ project2/app/src/main/java/com/example/c/t02_criminalintent/DatePickerFragment.java
@@ -0,0 +1,82 @@
+package com.example.c.t02_criminalintent;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by c on 2015-01-25.
+ */
+public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "com.example.c.t02_criminalintent.date";
+    Date mDate;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //return super.onCreateDialog(savedInstanceState);
+        mDate = (Date) getArguments().getSerializable(EXTRA_DATE);
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePicker datePicker = (DatePicker) v.findViewById(R.id.dialog_date_datePicker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(v);
+        builder.setTitle(R.string.date_picker_title);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                sendResult(Activity.RESULT_OK);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    private void sendResult(int resultCode) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+
+        Intent i = new Intent();
+        i.putExtra(EXTRA_DATE, mDate);
+
+        // setResult(resultCode, i);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, i);
+    }
+
+}
